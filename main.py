3b1a6a3691a492f4247b67632732e2542cd02c13
Farah Farchoukh <128323841+farahfarchoukh@users.noreply.github.com>
2024-12-29 20:43:52 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,150 @@
+import os
+import torch
+
+import albumentations
+from torchvision import models
+import numpy as np
+import pandas as pd
+import torch.nn as nn
+from sklearn import model_selection  # Import model_selection for StratifiedKFold
+
+from apex import amp
+from sklearn import metrics
+from torch.nn import functional as F
+
+from wtfml.data_loaders.image import ClassificationLoader
+from wtfml.engine import Engine
+from wtfml.utils import EarlyStopping
+
+class MobileNetV2Wrapper(nn.Module):
+    def __init__(self, pretrained=True):
+        super(MobileNetV2Wrapper, self).__init__()
+        self.model = models.mobilenet_v2(pretrained=pretrained)
+        in_features = self.model.classifier[1].in_features
+        self.model.classifier[1] = nn.Linear(in_features, 1)
+    
+    def forward(self, image, targets):
+        bs, _, _, _ = image.shape
+        x = self.model.features(image)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = x.reshape(bs, -1)
+        out = self.model.classifier(x)
+        loss = nn.BCEWithLogitsLoss()(
+            out, targets.reshape(-1, 1).type_as(out)
+        )
+        return out, loss
+
+def train(fold):
+    input_path = "/home/sudeep/workspace/melanoma/input/"
+    df = pd.read_csv(os.path.join(input_path, "train.csv"))
+    df["kfold"] = -1
+    df = df.sample(frac=1).reset_index(drop=True)
+    y = df.target.values
+    kf = model_selection.StratifiedKFold(n_splits=10)
+    for fold_, (_, v_) in enumerate(kf.split(X=df, y=y)):
+        df.loc[v_, "kfold"] = fold_
+
+    training_data_path = "/home/sudeep/workspace/melanoma/input/jpeg/train224/"
+    model_path = "/home/sudeep/workspace/melanoma-deep-learning"
+    device = "cuda"
+    epochs = 50
+    train_bs = 32
+    valid_bs = 16
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.224, 0.225)
+
+    # Use only the samples with the specified fold for training
+    df_train = df[df.kfold != fold].reset_index(drop=True)
+    df_valid = df[df.kfold == fold].reset_index(drop=True)
+
+    train_aug = albumentations.Compose(
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True),
+        ]
+    )
+
+    valid_aug = albumentations.Compose(
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True),
+        ]
+    )
+
+    train_images = df_train.image_name.values.tolist()
+    train_images = [os.path.join(training_data_path, i + ".jpg") for i in train_images]
+    train_targets = df_train.target.values
+
+    valid_images = df_valid.image_name.values.tolist()
+    valid_images = [os.path.join(training_data_path, i + ".jpg") for i in valid_images]
+    valid_targets = df_valid.target.values
+
+    train_dataset = ClassificationLoader(
+        image_paths=train_images,
+        targets=train_targets,
+        resize=None,
+        augmentations=train_aug
+    )
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=train_bs,
+        shuffle=True,
+        num_workers=4
+    )
+
+    valid_dataset = ClassificationLoader(
+        image_paths=valid_images,
+        targets=valid_targets,
+        resize=None,
+        augmentations=valid_aug
+    )
+
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset,
+        batch_size=valid_bs,
+        shuffle=False,
+        num_workers=4
+    )
+
+    model = MobileNetV2Wrapper(pretrained=True)
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer,
+        patience=3,
+        mode="max"
+    )
+
+    model, optimizer = amp.initialize(
+        model,
+        optimizer,
+        opt_level="O1",
+        verbosity=0
+    )
+
+    es = EarlyStopping(patience=5, mode="max")
+    for epoch in range(epochs):
+        training_loss = Engine.train(
+            train_loader, 
+            model,
+            optimizer,
+            device,
+            fp16=True
+        )
+        predictions, valid_loss = Engine.evaluate(
+            valid_loader, 
+            model,
+            optimizer,
+            device
+        )
+        predictions = np.vstack((predictions)).ravel()
+        auc = metrics.roc_auc_score(valid_targets, predictions)
+        scheduler.step(auc)
+        print(f"epoch={epoch}, auc={auc}")
+        es(auc, model, os.path.join(model_path, f"model{fold}.bin"))
+        if es.early_stop:
+            print("early stopping")
+            break
+
+# Use the train function for a specific fold
+train(fold=0)
